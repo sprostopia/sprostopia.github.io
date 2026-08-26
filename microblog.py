06cf2806c5a5e08f5e3b75a8510573e4faf69efb
sprostopia <ojspr2007@gmail.com>
2026-08-26 23:17:58 +0100

    Update index.html with welcome message; enhance microblog functionality and post creation script; remove unused files

--- microblog.py
+++ microblog.py
@@ -7,13 +7,25 @@ message = html.escape(input("Message:\n")).replace("\\n", "<br>")
 
 with open("docs/microblog/index.html", "r") as file:
     page = str(file.read())
+
+    #Gets the ID of the last post and adds one
+    newId = ""
+    offset = page.find("<!-- Next Post -->")#  Ensures that it's only searching for the first id of the entries, not any other html element
+    for digit in page[page[offset:].find("id=") + offset + 4:]:
+        if digit in "0123456789":
+            newId += digit
+        else:
+            break
+    newId = int(newId) + 1
+
+    #Creates the new post
     page = page.replace("<!-- Next Post -->",
     f"""<!-- Next Post -->
-    <div class="entry">
-        <h2>{title}</h2>
-        <h3><i>{datetime.datetime.now().strftime("%d/%m/%y, %H:%M:%S")}</i></h3>
-        <p>{message}</p>
-    </div>      
+        <div class="entry" id="{newId}">
+            <h2>{title}</h2>
+            <h3><i>{datetime.datetime.now().strftime("%d/%m/%y, %H:%M:%S")} </i><a href="#{newId}">Shareable link</a></h3>
+            <p>{message}</p>
+        </div>      
     """)
 with open("docs/microblog/index.html", "w") as file:
     file.write(page)
