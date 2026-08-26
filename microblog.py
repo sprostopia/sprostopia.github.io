3e4fe310b4fe2506aa27fd3180c9c5b44c1198cb
sprostopia <ojspr2007@gmail.com>
2026-08-26 22:23:46 +0100

    Fix typo in microblog link and clean up HTML structure in index.html; update microblog.py for improved formatting

--- microblog.py
+++ microblog.py
@@ -8,8 +8,7 @@ message = html.escape(input("Message:\n")).replace("\\n", "<br>")
 with open("docs/microblog/index.html", "r") as file:
     page = str(file.read())
     page = page.replace("<!-- Next Post -->",
-    f"""
-    <!-- Next Post -->
+    f"""<!-- Next Post -->
     <div class="entry">
         <h2>{title}</h2>
         <h3><i>{datetime.datetime.now().strftime("%d/%m/%y, %H:%M:%S")}</i></h3>
